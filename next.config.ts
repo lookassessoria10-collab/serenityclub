f3f639a4a65f03df806d1009ae222a13c142a2fe
lookassessoria10-collab <lookassessoria10@gmail.com>
2026-09-02 15:43:03 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    workerThreads: true,
-  },
 };
-
-export default nextConfig;
